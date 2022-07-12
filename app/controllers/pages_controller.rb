6ac449044c37425fe5c8558bc69fce1e79e99ca3
Reem Gawad <reemabdelg@gmail.com>
2022-07-12 15:57:55 -0400

    added static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!
 
   def home
+  end
+
+  def about
+  end
+
+  def brunchtime
+  end
+
+  def ruby_games
   end
 end
