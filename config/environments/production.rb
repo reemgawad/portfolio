16fb71bd22e498a3a7c4d1f193794ef36c303140
Reem Gawad <reemabdelg@gmail.com>
2022-07-23 15:04:27 +0200

    fix attempt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,12 +5,12 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'reemgawad.com', # UPDATE THIS VALUE WITH YOUR OWN APP
-    :authentication => :plain,
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'rgaw-portfolio.herokuapp.com',
+    authentication: :plain
   }
   ActionMailer::Base.delivery_method = :smtp
 
