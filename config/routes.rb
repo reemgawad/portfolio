1a2232a2531f0bcb3e9c656bc126fa10808346e3
Reem Gawad <reemabdelg@gmail.com>
2022-07-23 13:42:33 +0200

    set up mail_form, new + create routes views

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "brunchtime", to: "pages#brunchtime"
   get "ruby-games", to: "pages#ruby_games"
   get "contact", to: "pages#contact"
+  resources :contacts, only: [:new, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
