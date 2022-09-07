e270ac288ecc88e71a41838ba1500f591eabe1c9
Reem Gawad <reemabdelg@gmail.com>
2022-09-07 13:11:11 +0200

    removed unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get "about", to: "pages#about"
-  get "brunchtime", to: "pages#brunchtime"
-  get "ruby-games", to: "pages#ruby_games"
+  # get "about", to: "pages#about"
+  # get "brunchtime", to: "pages#brunchtime"
+  # get "ruby-games", to: "pages#ruby_games"
   get "contact", to: "pages#contact"
   resources :contacts, only: [:new, :create]
 
