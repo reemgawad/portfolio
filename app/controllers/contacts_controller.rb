cbe1243dfe15d7bf45dcd90be981eb9691d8476e
Reem Gawad <reemabdelg@gmail.com>
2022-09-12 14:03:46 +0200

    added contact form to home page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,10 @@
 class ContactsController < ApplicationController
   skip_before_action :authenticate_user!
   def new
-    @contact = Contact.new
   end
 
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
     @contact.request = request
     if @contact.deliver
       flash.now[:success] = 'Message sent!'
@@ -13,5 +12,11 @@ class ContactsController < ApplicationController
       flash.now[:error] = 'Could not send message'
       render :new
     end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
+    @contact = Contact.new
   end
 
   def about
